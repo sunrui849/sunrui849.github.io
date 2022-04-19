9d371608946e1fbb1ed5bbcec7121a151e20b006
sunrui62 <sunrui62@jd.com>
2022-04-19 18:02:22 +0800

    init

---
+++ reset_md.sh
@@ -0,0 +1,30 @@
+#echo 'hhh'
+base_dir="./docs"
+echo "个人博客" > README.md
+function str_num(){
+i=1
+res=""
+while(( $i <= $1 ))
+do
+  res=$res"#"
+  i=$i+1
+done
+}
+
+function read_dir(){
+for file in `ls $1` 
+  do
+    if [ -d $1"/"$file ]
+    then
+      str_num $2     
+      echo "$res $file" >> README.md
+      read_dir $1"/"$file $2+1
+    else
+      echo $1"/"$file
+      echo "* $file" >> README.md
+    fi
+  done 
+}
+
+read_dir $base_dir 2
+
